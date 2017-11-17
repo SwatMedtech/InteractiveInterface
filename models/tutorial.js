d27888fafad687514ddefa15cdab02e057f5a801
imenfaith <imen.jaouadi2@gmail.com>
2017-11-17 17:59:51 +0100

    routes/tutorial models/tutorial
    routes/tutorial
    models/tutorial

---
+++ models/tutorial.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+const tutorialSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    number:{
+        type:Number,
+        required:true
+    },
+    labnumber :{},
+    labcourse :{
+    },
+
+    content:{
+        type: String,
+        required:true
+    },
+
+});
+
+const Tutorial = module.exports = mongoose.model('Tutorial', tutorialSchema);
+
+module.exports.getTutorialById = function(id, callback){
+    Tutorial.findById(id, callback);
+};
+
+module.exports.getTutorialByName = function(name, callback){
+    const query = {name:name}
+    Tutorial.findOne(query, callback);
+};
+module.exports.selectAll = function (callback) {
+    tutorialSchema.find();
+
+};
+module.exports.addTutorial= function (newTutorial,callback) {
+
+    newTutorial.save(callback);
+
+};
---
+++ routes/lab_course.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const labCourse = require('./lab_course');
+
+router.post('/labCourse',(req,res,next)=>{
+    const newLabCourse=new labCourse({
+        name:req.body.name,
+        content:req.body.content,
+        number: req.body.number
+    });
+    labCourse.addLab(newLabCourse,(err)=>{
+        if(err){
+            res.json({success:false, msg:'failed to add new lab course !!'});
+        }else { res.json({success:true, msg:'success to add new lab course !!'});
+        }
+    });
+});
+
+module.exports=router;
