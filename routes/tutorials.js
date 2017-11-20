c5fe79dd31c090cb9c931d48d17c1fff9376049c
imenfaith <imen.jaouadi2@gmail.com>
2017-11-20 19:04:43 +0100

    Tutorial router created

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const tutorial = require('./tutori');
+const tutorial = require('./tutorial');
 
-router.post('/labCourse',(req,res,next)=>{
-    const newLabCourse=new labCourse({
+router.post('/tutorial',(req,res,next)=>{
+    const newTutorial=new tutorial({
         name:req.body.name,
         content:req.body.content,
         number: req.body.number
     });
-    labCourse.addLab(newLabCourse,(err)=>{
+    tutorial.addLab(newLabCourse,(err)=>{
         if(err){
             res.json({success:false, msg:'failed to add new lab course !!'});
         }else { res.json({success:true, msg:'success to add new lab course !!'});
