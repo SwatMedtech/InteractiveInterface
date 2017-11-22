ecf81b3ba0745441234a743b0be095b68c79e725
Belguith Omar <omar.belguith@medtech.tn>
2017-11-22 19:44:44 +0100

    labcourse change

--- angular-src/src/app/lab-reports/lab-reports.component.ts
+++ angular-src/src/app/lab-reports/lab-reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RequestService} from "../services/request.service";
 
 @Component({
   selector: 'app-lab-reports',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabReportsComponent implements OnInit {
 
-  constructor() { }
-
+  labName: string;
+  description: string;
+  number: number;
+  constructor(private request: RequestService) { }
+  onSubmit() {
+    const labCourse  = {
+      labName: this.labName,
+      description: this.description,
+      number: this.number
+    };
+    this.request.labCoursesPost(labCourse).subscribe(data =>{
+      console.log(data);
+    });
+  }
+  increment() {
+    this.number = this.number + 1 ;
+  }
+  decrement() {
+    this.number = this.number - 1;
+  }
   ngOnInit() {
+    this.number = 1;
   }
 
 }
